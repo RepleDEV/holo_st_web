8af4ddd95b09959b18e04cbd6032e104d2d11cdf
Gonpachiro Kamaboko <54616266+RepleDEV@users.noreply.github.com>
2021-03-10 07:45:37 +0700

    Filter Streams

--- src/app/modules/streamdisplay.ts
+++ src/app/modules/streamdisplay.ts
@@ -4,6 +4,7 @@ import { MinimizedStreams } from "../../core/globals";
 import { Generation } from "../../core/modules/holo_st/globals";
 
 import streamCardCreator from "./streamcardcreator";
+import moment from "moment";
 
 export default class StreamDisplay {
     cards: string[] = [];
@@ -35,6 +36,19 @@ export default class StreamDisplay {
 
         for (let i = 0;i < this.streams.upcomingStreams.length;i++) {
             const stream = this.streams.upcomingStreams[i];
+
+            // This checks whether or not the stream is scheduled within the next day.
+            // I'm not sorry that this is a 1 liner.
+            const startOfNextDay = new Date(
+                new Date().setHours(0, 0, 0, 0)
+            ).setDate(new Date().getDate() + 1);
+            const twentyFourHours = 1000 * 60 * 60 * 24;
+
+            if (+moment(stream.scheduledStartTime) >= startOfNextDay + twentyFourHours) {
+                // Break because it's GUARANTEED that every other stream after this
+                // is scheduled after the 2 day filter.
+                break;
+            }
 
             const streamCardPromise = streamCardCreator(
                 stream.title,
