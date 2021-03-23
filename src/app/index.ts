2ec6adb7b4687497120c6a5faa85f245985dac47
Gonpachiro Kamaboko <54616266+RepleDEV@users.noreply.github.com>
2021-03-23 12:47:35 +0700

    Sidepanel animation using animejs

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,6 +1,7 @@
 import "../../public/scss/base.scss";
 
 import $ from "jquery";
+import anime from "animejs";
 
 import { Generation } from "../core/modules/holo_st/globals";
 import StreamDisplay from "./modules/streamdisplay";
@@ -93,6 +94,59 @@ async function load_icons(): Promise<void> {
     }
 }
 
+function toggle_sidepanel(): void {
+    const sidePanel = $("nav.side-navbar");
+    const width = sidePanel.width();
+    const isHidden = sidePanel.hasClass("hidden");
+
+    const translateX = [`-${width}px`, 0];
+    const easing = "cubicBezier(0.650, 0.115, 0.375, 0.900)";
+    const duration = 300;
+
+    if (isHidden) {
+        sidePanel.removeClass("hidden");
+
+        // Disable toggle button.
+        $(".nav-panel-toggle-container").prop("disable", true);
+
+        anime({
+            targets: "nav.side-navbar",
+            translateX: translateX,
+            duration: duration,
+            easing: easing
+        });
+        
+        $(".side-navbar-overlay").removeClass("hidden");
+
+        anime({
+            targets: ".side-navbar-overlay",
+            opacity: 1,
+            duration: duration,
+            easing: easing
+        });
+    } else {
+        anime({
+            targets: "nav.side-navbar",
+            translateX: translateX.reverse(),
+            duration: duration,
+            easing: easing
+        }).finished.then(() => {
+            // Re-enable toggle button
+            $(".nav-panel-toggle-container").prop("disable", false);
+
+            sidePanel.addClass("hidden");
+        });
+        anime({
+            targets: ".side-navbar-overlay",
+            opacity: 0,
+            duration: duration,
+            easing: easing
+        }).finished.then(() => {
+            $(".side-navbar-overlay").addClass("hidden");
+        });
+    }
+}
+
 $(async () => {
     await load_icons();
 
@@ -103,15 +157,9 @@ $(async () => {
     await streamDisplay.init(minimized_streams);
     streamDisplay.display();
 
-    $(".nav-panel-toggle-container").on("click", () => {
-        $("nav.side-navbar").toggleClass("hidden");
-        $(".side-navbar-overlay").toggleClass("hidden");
-    });
+    $(".nav-panel-toggle-container").on("click", toggle_sidepanel);
 
-    $(".side-navbar-overlay > .exit-button-container").on("click", () => {
-        $("nav.side-navbar").toggleClass("hidden");
-        $(".side-navbar-overlay").toggleClass("hidden");
-    });
+    $(".side-navbar-overlay > .exit-button-container").on("click", toggle_sidepanel);
 
     $(".side-navbar .dropdown-container > .button").on("click", (e) => {
         const element = $(e.target);
