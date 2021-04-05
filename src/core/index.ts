2d7a5f916f2dff44c6c6efe242432277a742bc22
Gonpachiro Kamaboko <54616266+RepleDEV@users.noreply.github.com>
2021-04-05 13:25:10 +0700

    Temporary testing code

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import path from "path";
+import puppeteer from "puppeteer";
 
 import * as routes from "./modules/routes";
 import { StreamList } from "./modules/stream_list";
 import { list_streams } from "./modules/list_streams";
 import { init } from "./modules/listeners";
+import { get_html } from "./modules/holo_st/modules/get_html";
 
 const app = express();
 const PORT = process.env.PORT || 9106;
@@ -12,24 +14,36 @@ const PORT = process.env.PORT || 9106;
 const streamList = new StreamList();
 
 (async () => {
-    console.log("Starting server.");
+    // console.log("Starting server.");
     app.listen(PORT, () => {
         console.log("App Started!");
     });
 
-    console.log("Caching streams...");
+    // console.log("Caching streams...");
 
-    await list_streams(streamList);
-    console.log("Finished caching streams.");
+    // await list_streams(streamList);
+    // console.log("Finished caching streams.");
 
-    console.log("Initializing listeners.");
-    init(streamList);
-    console.log("Finished initializing listeners");
+    // console.log("Initializing listeners.");
+    // init(streamList);
+    // console.log("Finished initializing listeners");
 
     // Routes
-    app.use("/public", express.static(path.resolve("./public")));
-    app.get("/", routes.home);
-    app.get("/streams", routes.streams(streamList));
+    // app.use("/public", express.static(path.resolve("./public")));
+
+    // TESTING
+    const browser = await puppeteer.launch({
+        args: [
+            "--no-sandbox",
+            "--disable-setuid-sandbox"
+        ]
+    });
+    const TESTHTML = await get_html("https://www.youtube.com/channel/UCAWSyEs_Io8MtpY3m-zqILA", browser);
+    await browser.close();
+    app.get("/", (req, res) => {
+        res.send(TESTHTML);
+    });
+    // app.get("/streams", routes.streams(streamList));
 
     // 404 redirect. ALWAYS KEEP THIS AT THE BACK. (things will go wrong~)
     app.use(routes.redirect);
