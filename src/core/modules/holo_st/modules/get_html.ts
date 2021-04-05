2556559faae3daeda7a6cb9a1cb7d1cc759fe37f
Gonpachiro Kamaboko <54616266+RepleDEV@users.noreply.github.com>
2021-04-05 14:23:22 +0700

    Attempt fix issue: Manually clicking the 'Accept' button

--- src/core/modules/holo_st/modules/get_html.ts
+++ src/core/modules/holo_st/modules/get_html.ts
@@ -8,24 +8,28 @@ export async function get_html(
     const page = await browser.newPage();
 
     await page.setRequestInterception(true);
-    page.on("request", (req) => {
-        if (req.isNavigationRequest() && req.redirectChain().length !== 0) {
-            req.abort();
-        } else {
-            req.continue();
-        }
-    });
 
     await page.setUserAgent(
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36"
     );
     await page.goto(url, { waitUntil: "networkidle0" });
+
+    async function checkUrl() {
+        let currentURL = (await page.evaluate(() => window.location.href));
+
+        if (currentURL !== url && currentURL.includes("consent.youtube.com")) {
+            await page.click("div.VfPpkd-RLmnJb");
+            console.log(currentURL);
+            setTimeout(async () => { await checkUrl() }, 250);
+        }
+    }
+
+    await checkUrl();
+
     const data =
         (await page.evaluate(
             () => (document.querySelector("*") || {}).outerHTML
         )) || "";
-    const currentURL = (await page.evaluate(() => window.location.href))
-    console.log(currentURL);
     // Don't close the browser if it's imported but close the page.
     if (browser_p) {
         await page.close();
