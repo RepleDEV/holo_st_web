ff77915d4aeeba7780602a33fcd4415472bee10c
Gonpachiro Kamaboko <54616266+RepleDEV@users.noreply.github.com>
2021-04-28 13:14:51 +0700

    Add more variables to ongoingStreamListener logging.

--- src/core/modules/listeners.ts
+++ src/core/modules/listeners.ts
@@ -40,7 +40,7 @@ async function ongoingStreamCallback(
         }
     }
 
-    console.log(`ongoingStreamCallback log: stream_info.items.length: ${stream_info.items.length}`)
+    console.log(`ongoingStreamCallback log: stream_info.items.length: ${stream_info.items.length}, ID: ${id}, isStreaming: ${isStreaming}.`);
 
     if (!isStreaming) {
         cache.removeOngoingStream(id);
