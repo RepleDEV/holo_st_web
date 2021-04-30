96db77f0e1c9b3928403cd973ad55d1344d94257
Gonpachiro Kamaboko <54616266+RepleDEV@users.noreply.github.com>
2021-04-30 20:15:19 +0700

    Cleaned code

--- src/core/modules/holo_st/index.ts
+++ src/core/modules/holo_st/index.ts
@@ -1,86 +1,21 @@
 import puppeteer, { Browser } from "puppeteer";
 
 import { OngoingStream, UpcomingStream } from "./globals";
-import { get_upcoming_streams } from "./modules/get_upcoming_streams";
-import { get_ongoing_streams } from "./modules/get_ongoing_streams";
 import { get_channels } from "./modules/get_channels";
 import get_streams from "./modules/get_streams";
 
-export async function get_all_upcoming_streams(
-    filter: string[] = [],
-    check_callback?: (upcoming_streams: UpcomingStream[], i: number) => void
-): Promise<UpcomingStream[]> {
-    filter = [...new Set(filter)];
+// TODO: Write these functions :)
+// export async function get_all_upcoming_streams(
+//     check_callback?: (upcoming_streams: UpcomingStream[], i: number) => void
+// ): Promise<UpcomingStream[]> {
+//     return [];
+// }
 
-    const channels = await get_channels();
-
-    const browser = await puppeteer.launch({
-        args: [
-            "--no-sandbox",
-            "--disable-setuid-sandbox"
-        ]
-    });
-
-    const res: UpcomingStream[] = [];
-
-    if (filter.length) channels.filter((x) => filter.includes(x.channel.id));
-
-    for (let i = 0; i < channels.length; i++) {
-        const channelId = channels[i].channel.id;
-
-        const upcomingStreams = await get_upcoming_streams(channelId, browser);
-        res.push(...upcomingStreams);
-
-        if (typeof check_callback === "function") {
-            check_callback(upcomingStreams, i);
-        }
-    }
-
-    await browser.close();
-
-    return res;
-}
-
-export async function get_all_ongoing_streams(
-    filter: string[] = [],
-    check_callback?: (ongoing_streams: OngoingStream[], i: number) => void
-): Promise<OngoingStream[]> {
-    filter = [...new Set(filter)];
-
-    const channels = await get_channels();
-    // If the environment is production, pass no argument to puppeteer.
-    const browser = await puppeteer.launch(
-        (
-            process.env.NODE_ENV === "production" ? 
-            {
-                args: [
-                    "--no-sandbox",
-                    "--disable-setuid-sandbox"
-                ]
-            } :
-            undefined
-        )
-    );
-
-    const res: OngoingStream[] = [];
-
-    if (filter.length) channels.filter((x) => filter.includes(x.channel.id));
-
-    for (let i = 0; i < channels.length; i++) {
-        const channelId = channels[i].channel.id;
-
-        const ongoingStreams = await get_ongoing_streams(channelId, browser);
-        res.push(...ongoingStreams);
-
-        if (typeof check_callback === "function") {
-            check_callback(ongoingStreams, i);
-        }
-    }
-
-    await browser.close();
-
-    return res;
-}
+// export async function get_all_ongoing_streams(
+//     check_callback?: (ongoing_streams: OngoingStream[], i: number) => void
+// ): Promise<OngoingStream[]> {
+//     return [];
+// }
 
 // TODO: Move this function to get_browser.ts in modules/ to allow imports
 async function get_browser(): Promise<Browser> {
--- src/core/modules/holo_st/modules/parse_time.ts
+++
@@ -1,5 +0,0 @@
-import moment, { MomentObjectOutput } from "moment";
-
-export function parse_time(time: string): MomentObjectOutput {
-    return moment(time).toObject();
-}
